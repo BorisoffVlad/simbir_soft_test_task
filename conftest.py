c5121373c06097d65e97d27028bd7cd30b6e502e
BorisoffVlad <vlad.borisoff1994@gmail.com>
2024-05-19 09:57:28 +0400

    fix

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption("--headless", action="store", default=False)
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--executor", action="store", default="http://10.1.0.10:4444")
 
 
 @pytest.fixture(scope="function")
@@ -14,13 +13,11 @@ def browser(request):
     headless = request.config.getoption("--headless")
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
-        options.add_argument("--executor")
         if headless:
             options.add_argument("--headless")
         browser = webdriver.Remote(options=options)
     elif browser_name == "firefox":
         options = webdriver.FirefoxOptions()
-        options.add_argument("--executor")
         if headless:
             options.add_argument("--headless")
         browser = webdriver.Remote(options=options)
